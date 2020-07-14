81bdc3e26c8d6e04f99f433af64c9e7d02e66653
ehgml961017 <ehgml961017@naver.com>
2020-07-14 17:47:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/smartvalve/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.smartvalve
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME:Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+}
